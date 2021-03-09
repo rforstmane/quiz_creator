9042951c082468df224b89de942bfa80ad5bf1be
Renda Forstmane <renda.forstmane@bright.lv>
2021-03-09 14:52:40 +0200

    Returned all survey results

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,12 +1,24 @@
 <template>
   <div>Aizpildītas
+    <SurveyResults
+        v-for="(answers, index) in allAnswers"
+        :key="index"
+        :answers="answers"
+        :answersID="index"
+    />
   </div>
-
 </template>
 
 <script>
 
+import SurveyResults from "@/components/results/SurveyResults";
 export default {
-  name: 'Results'
+  name: 'Results',
+  components: {SurveyResults},
+  data() {
+    return {
+      allAnswers: JSON.parse(localStorage.getItem("allAnswers")),
+    }
+  },
 }
 </script>
